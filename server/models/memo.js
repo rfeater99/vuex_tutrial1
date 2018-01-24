ee39690de93ea28f698063054277828a5f0050db
rfeather99 <rfeather99@yahoo.co.jp>
2018-01-25 07:09:16 +0900

    serverサイド作成中

--- server/models/memo.js
+++ server/models/memo.js
@@ -1,41 +1,43 @@
+'use strict'
+
+// (a)使用モジュールの読み込み
+var cradle = require('cradle')
+
+// (b)Cloudant接続情報の取得
+var services = JSON.parse(process.env.VCAP_SERVICES)
+var credentials = services['cloudantNoSQLDB'][0].credentials
+var host = credentials.host
+var port = credentials.port
+var options = {
+  cache: true,
+  raw: false,
+  secure: true,
+  auth: {
+    username: credentials.username,
+    password: credentials.password
+  }
+}
+
+// (c)メモ・データを保持するデータベース
+var db = new (cradle.Connection)(host, port, options)
+            .database('memo')
+
+// (1)メモ一覧の取得
 exports.list = function (callback) {
-    callback(null, [{
-        _id: '12345678-1234-1234-1234-123456789001',
-        _rev: 'ake2234aeadfsa',
-        title: 'xxx',
-        content: 'bbb',
-        updateAt: '2017/12/19 10:00'
-      },
-      {
-        _id: '12345678-1234-1234-1234-123456789002',
-        _rev: 'ake2234aeadfsa',
-        title: 'xxx2',
-        content: 'bbb2',
-        updateAt: '2017/12/19 11:00'
-      },
-      {
-        _id: '12345678-1234-1234-1234-123456789003',
-        _rev: 'ake2234aeadfsa',
-        title: 'xxx3',
-        content: 'bbb3',
-        updateAt: '2017/12/20 14:00'
-      }]);
-};
+  db.view('memos/list', { descending: true }, callback)
+}
 
+// (2)メモの取得
 exports.get = function (id, callback) {
-    callback(null, {
-      _id: '12345678-1234-1234-1234-1234567890001',
-      _rev: 'ake2234aeadfsa',
-      title: 'xxx3',
-      content: 'bbb3',
-      updateAt: '2017/12/20 14:00'
-    });
+  db.get(id, callback)
 }
 
+// (3)メモの保存
 exports.save = function (id, doc, callback) {
-    callback(null);
+  db.save(id, doc, callback)
 }
 
-exports.delete = function (id, callback) {
-    callback(null);
+// (4)メモの削除
+exports.remove = function (id, callback) {
+  db.remove(id, callback)
 }
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,65 +1,67 @@
-const express = require('express');
-const router = express.Router();
-const memo = require('../models/memo');
-const uuid = require('node-uuid');
-const moment = require('moment');
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const memo = require('../models/memo')
+const uuid = require('node-uuid')
+const moment = require('moment')
 
 router.get('/memos', (req, res) => {
-  console.log('call memos!');
+  console.log('call memos!')
   memo.list(function (err, list) {
     if (err) {
-        res.status(500);
+      res.status(500)
     }
-    res.status(200).json(list);
-  });
+    res.status(200).json(list)
+  })
 })
 
 router.get('/memos/:id([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', (req, res) => {
-  console.log('call memos/:id!');
+  console.log('call memos/:id!')
   memo.get(req.params['id'], (err, doc) => {
     if (err) {
-      res.status(500);
+      res.status(500)
     }
-    res.status(200).json(doc);
-  });
-});
+    res.status(200).json(doc)
+  })
+})
 
 router.post('/memos', (req, res) => {
-  console.log('call memos post!');
+  console.log('call memos post!')
   memo.save(uuid.v4(), {
-    title : req.body.title,
-    content : req.body.content,
-    updatedAt : moment().zone('+0900').format('YYYY/MM/DD HH:mm:ss')
+    title: req.body.title,
+    content: req.body.content,
+    updatedAt: moment().zone('+0900').format('YYYY/MM/DD HH:mm:ss')
   }, (err) => {
-      if (err) {
-        res.status(500);
-      }
-      res.status(200);
-    });
-});
+    if (err) {
+      res.status(500)
+    }
+    res.status(200)
+  })
+})
 
 router.put('/memos/:id([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', (req, res) => {
-  console.log('call memos/:id put');
+  console.log('call memos/:id put')
   memo.save(req.params['id'], {
-    title : req.body.title,
-    content : req.body.content,
-    updatedAt : moment().zone('+0900').format('YYYY/MM/DD HH:mm:ss')
+    title: req.body.title,
+    content: req.body.content,
+    updatedAt: moment().zone('+0900').format('YYYY/MM/DD HH:mm:ss')
   }, (err) => {
     if (err) {
-        res.status(500);
+      res.status(500)
     }
-    res.status(200);
-  });
-});
+    res.status(200)
+  })
+})
 
 router.delete('/memos/:id([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', (req, res) => {
-  console.log('call memos/:id delete!');
+  console.log('call memos/:id delete!')
   memo.remove(req.params['id'], (err) => {
     if (err) {
-      res.status(500);
+      res.status(500)
     }
-    res.status(200); 
-  });
-});
+    res.status(200)
+  })
+})
 
-module.exports = router;
+module.exports = router
